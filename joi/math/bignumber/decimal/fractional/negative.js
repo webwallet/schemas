7fe6bafea9b9b0b3836a4ed76a55eeb0df42fc2a
Jorge Zaccaro <jorgezaccaro@gmail.com>
2018-06-04 23:40:59 +0800

    feat(joi): added big-number schemas

---
+++ joi/math/bignumber/decimal/fractional/negative.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const joi = require('joi')
+
+const {config} = global
+const bigNumber = config.math.bignumber
+const regex = new RegExp(bigNumber.regex.decimal.fractional.negative)
+
+const schema = joi.string().regex(regex).max(bigNumber.length.fractional)
+  .description('big-number string for arbitrary-precision arithmetic')
+
+module.exports = schema
---
+++ joi/math/bignumber/decimal/integer/positive.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const joi = require('joi')
+
+const {config} = global
+const bigNumber = config.math.bignumber
+const regex = new RegExp(bigNumber.regex.decimal.integer.positive)
+
+const schema = joi.string().regex(regex).max(bigNumber.length.integer)
+  .description('big-number string for arbitrary-precision arithmetic')
+
+module.exports = schema
