d047fccf58dab456d61700622f1454fc6e368ba4
Jorge Zaccaro <jorgezaccaro@gmail.com>
2018-06-04 21:17:09 +0800

    refactored config.json hierarchy

--- joi/crypto/address/string.js
+++ joi/crypto/address/string.js
@@ -4,7 +4,7 @@ const joi = require('joi')
 
 const {config} = global
 const base58 = new RegExp(config.regex.base58)
-const {min, max} = config.lengths.crypto.address
+const {min, max} = config.crypto.address.length
 
 const schema = joi.string().alphanum().regex(base58).min(min).max(max)
   .description('identifier for a cryptographic encumbrance')
--- joi/crypto/hash/object.js
+++ joi/crypto/hash/object.js
@@ -12,6 +12,6 @@ const schema = joi.object().keys({
   typ: joi.string().valid(generation).default(generation[0])
     .description('colon-separated list of instructions for hash generation'),
   val: hashStringSchema.required()
-}).description('set of properties for describing a cryptographic hash value')
+}).description('properties for describing a cryptographic hash value')
 
 module.exports = schema
