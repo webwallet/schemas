fb6cc97fa797505a50ca9e8b6b5b4a1913822e5b
Jorge Zaccaro <jorgezaccaro@gmail.com>
2018-06-05 20:17:42 +0800

    feat(joi): added schema for transaction events

---
+++ joi/transaction/event/array.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const joi = require('joi')
+
+const {config, schemas} = global
+const eventSchema = schemas.crypto.hash.string
+const eventsArrayItems = config.transaction.events.array
+
+const schema = joi.array().items(eventSchema).unique()
+  .min(eventsArrayItems.min).max(eventsArrayItems.max)
+  .description('array of event identifiers')
+  .options({stripUnknown: false})
+
+module.exports = schema
