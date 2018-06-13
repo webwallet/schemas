0f46b31c55e366e1545903091846513cce22cc38
Jorge Zaccaro <jorgezaccaro@gmail.com>
2018-06-13 18:28:20 +0800

    refactor(iou): renamed IOU properties and replaced sigs->meta

--- joi/transaction/iou/data.js
+++ joi/transaction/iou/data.js
@@ -5,33 +5,31 @@ const joi = require('joi')
 const {config, schemas} = global
 const addressStringSchema = schemas.crypto.address.string
 const countspaceStringSchema = schemas.crypto.countspace.string
-const issuerDomainLength = config.transaction.request.iou.domain
-const iouNonceLength = config.transaction.request.iou.nonce
+const clearingDomainLength = config.transaction.request.iou.domain
+const iouRandomStringLength = config.transaction.request.iou.random
 const bigNumberStringSchemas = schemas.math.bignumber.decimal.fractional
 
 const schema = joi.object().keys({
-  iss: joi.string().trim().max(issuerDomainLength.max).required()
-    .description('(issuer) transaction clearing domain'),
-  sub: addressStringSchema.required()
-    .description('(subject) source of the transaction'),
-  aud: addressStringSchema.required()
-    .description('(audience) destination of the transaction'),
+  domain: joi.string().trim().max(clearingDomainLength.max).required()
+    .description('transaction clearing domain'),
+  source: addressStringSchema.required()
+    .description('source address sending the units'),
+  target: addressStringSchema.required()
+    .description('target address to receive the units'),
 
-  amt: bigNumberStringSchemas.positive.required()
-    .description('(amount) number of units to offset on clearing'),
-  alw: bigNumberStringSchemas.negative
-    .description('(allowance) number of units to grant as credit'),
-  cru: countspaceStringSchema.required()
-    .description('(unit of account) cryptographic countspace identifier'),
-  nce: joi.string().max(iouNonceLength.max).required()
-    .description('(nonce) random value to prevent replay attacks'),
+  amount: bigNumberStringSchemas.positive.required()
+    .description('number of units to offset/transfer on transaction clearing'),
+  credit: bigNumberStringSchemas.negative.optional()
+    .description('number of units to grant as credit in the balance.min property'),
+  symbol: countspaceStringSchema.required()
+    .description('symbol of a unit of account (countspace identifier)'),
 
-  iat: joi.date().iso().max('now').options({convert: true})
-    .description('(issued at) for reference purpose only'),
-  nbf: joi.date().iso().options({convert: true})
-    .description('(not before) earliest valid date for clearing'),
-  exp: joi.date().iso().required().options({convert: true})
-    .description('(expires) latest valid date for clearing')
+  random: joi.string().max(iouRandomStringLength.max).required()
+    .description('random value for preventing replay attacks'),
+  active: joi.date().iso().optional().options({convert: true})
+    .description('date when the IOU becomes active for clearing'),
+  expiry: joi.date().iso().required().options({convert: true})
+    .description('latest valid date for clearing the IOU'),
 })
 
 module.exports = schema
--- joi/transaction/iou/object.js
+++ joi/transaction/iou/object.js
@@ -7,10 +7,15 @@ const iouDataObjectSchema = require('./data')
 const cryptoHashObjectSchema = schemas.crypto.hash.object
 const cryptoSignaturesArraySchema = schemas.crypto.signature.array
 
+const iouObjectMetadataSchema = joi.object().keys({
+  signatures: cryptoSignaturesArraySchema.required()
+}).description('metadata that is neither hashed nor signed')
+
+
 const schema = joi.object().keys({
   hash: cryptoHashObjectSchema.required(),
   data: iouDataObjectSchema.required(),
-  sigs: cryptoSignaturesArraySchema.required()
+  meta: iouObjectMetadataSchema.required()
 })
 
 module.exports = schema
