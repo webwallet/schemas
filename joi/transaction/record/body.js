3c4a7c5323c0489ce580abbd9acd607c960b6059
Jorge Zaccaro <jorgezaccaro@gmail.com>
2018-06-13 18:29:44 +0800

    feat(joi): added inputs property to transaction record

--- joi/transaction/record/body.js
+++ joi/transaction/record/body.js
@@ -3,11 +3,13 @@
 const joi = require('joi')
 
 const {schemas} = global
+const inputsArraySchema = require('../request/inputs')
 const transactionRecordDataSchema = require('./data')
 const cryptoHashObjectSchema = schemas.crypto.hash.object
 const cryptoSignaturesArraySchema = schemas.crypto.signature.array
 
 const transactionRecordMetadata = joi.object().keys({
+  inputs: inputsArraySchema.required(),
   signatures: cryptoSignaturesArraySchema.optional()
 }).description('metadata that is neither hashed nor signed')
 
