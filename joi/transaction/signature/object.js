bda11e3e52a647d211e461cf40cdd2b1be47371e
Jorge Zaccaro <jorgezaccaro@gmail.com>
2019-03-16 17:57:02 +0800

    properties: removed "keypos", added "script"; updated config require

--- joi/transaction/signature/object.js
+++ joi/transaction/signature/object.js
@@ -2,31 +2,28 @@
 
 const joi = require('joi')
 
+const config = require('*joi/config')
+const publicKeyStringSchema = require('*joi/crypto/keys/public/string')
+const validDigitalSignatureSchemes = require('*joi/crypto/signature/schemes')
+const signatureScriptIdentifierSchema = require('*joi/crypto/hash/string')
 const addressStringSchema = require('../address/string')
-const publicKeyStringSchema = require('../keys/public/string')
 const signatureStringSchema = require('./string')
 
-const {config} = global
-const {address, signature} = config.crypto
-const addressDerivation = address.derivation
-const maxPublicKeys = address.keys.array.max
-const {schemes} = signature
+const addressDerivation = config.crypto.address.derivation
 
 const schema = joi.object().keys({
-  scheme: joi.string().valid(schemes).required()
+  scheme: validDigitalSignatureSchemes.required()
     .description('digital signature scheme (algorithm-curve)'),
   signer: addressStringSchema.required()
     .description('wallet address providing the signature'),
   string: signatureStringSchema.required()
     .description('digital signature string value in hex format'),
-
-  public: publicKeyStringSchema
+  public: publicKeyStringSchema.required()
     .description('public key for signature verification'),
-  keypos: joi.number().integer().min(0).max(maxPublicKeys - 1)
-    .description('position in an array of public keys'),
-  linker: joi.string().valid(addressDerivation).default(addressDerivation[0])
-    .description('address derivation function for linking to the public key')
-}).or('public', 'keypos')
-.description('properties for describing a digital signature')
+  script: signatureScriptIdentifierSchema.optional()
+    .description('script containing rules for transaction clearing'),
+  linker: joi.string().valid(addressDerivation).default('?')
+    .description('derivation function that relates public/script to signer')
+}).description('properties for describing a digital signature')
 
 module.exports = schema
