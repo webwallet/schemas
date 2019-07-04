64d6e682cba28c05b2d5aedb176849f4aa853bdd
Jorge Zaccaro <jorgezaccaro@gmail.com>
2019-07-05 00:55:06 +0800

    added 'custom' object to transaction request IOU meta property

--- formats/joi/transaction/input/object.js
+++ formats/joi/transaction/input/object.js
@@ -1,13 +1,19 @@
 'use strict'
 
 const joi = require('joi')
+const config = require('*joi/config')
 
 const cryptoHashObjectSchema = require('*joi/crypto/hash/object')
 const cryptoSignaturesArraySchema = require('../signature/array')
 const iouDataObjectSchema = require('./data')
+const maxCustomDataKeys = config.transaction.request.iou.custom.keys.max
 
 const iouMetaObjectSchema = joi.object().keys({
-  signatures: cryptoSignaturesArraySchema.required()
+  signatures: cryptoSignaturesArraySchema.required(),
+  custom: {
+    hash: cryptoHashObjectSchema.optional(),
+    data: joi.object().max(maxCustomDataKeys).required()
+  }
 }).description('metadata that is neither hashed nor signed')
 
 
